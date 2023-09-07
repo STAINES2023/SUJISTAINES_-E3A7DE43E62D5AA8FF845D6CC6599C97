d27b811bd34af32cb8128ad4847e0c49efe4610d
STAINES2023 <144322219+STAINES2023@users.noreply.github.com>
2023-09-07 13:31:23 +0530

    Add files via upload

---
+++ Challenges_Unit 1/challange 1.2.py
@@ -0,0 +1,8 @@
+# online python compiler (interpreter) ro run python online.
+# write python 3 code in this online editor and run it.
+year = 2023
+if(year%4 == 0):
+  Print("Leap year")
+else:
+  print("Not Leap year")
+  
